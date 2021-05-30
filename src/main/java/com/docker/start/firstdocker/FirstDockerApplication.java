f5d42ab195a7c5ee6de6356cfe5bd26a9b08213b
udayprakashmishra <udayprakashmishra@hotmail.com>
2021-05-30 20:41:26 +0530

    Update FirstDockerApplication.java

--- src/main/java/com/docker/start/firstdocker/FirstDockerApplication.java
+++ src/main/java/com/docker/start/firstdocker/FirstDockerApplication.java
@@ -4,6 +4,7 @@ package com.docker.start.firstdocker;
 public class FirstDockerApplication {
 
 	public static void main(String[] args) {
+		
 
 		System.out.println("Docker Jenking Integration");
 	}
